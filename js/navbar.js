5c72e7441ef5162d1785c1f7afcd837ba625a6a6
Joe Sorensen <joseph.sorensen@flyfrontier.com>
2019-09-10 15:44:42 -0600

    Finalized some stuff

--- js/navbar.js
+++ js/navbar.js
@@ -1,19 +1,18 @@
 Vue.component("navbar", {
     template: "<div style=\"\n" +
         "        background-image: url('cdn/splash.png');\n" +
-        "\n" +
         "        /* Center and scale the image nicely */\n" +
-        "        background-position: center;\n" +
         "        background-repeat: no-repeat;\n" +
         "        background-size: cover;\n" +
-        "\n" +
+        "        background-position-y: -250px;" +
+        "        background-position: bottom" +
         "        /* Needed to position the navbar */\n" +
         "        position: relative;\n" +
         "    \">\n" +
         "        <nav style=\"background: linear-gradient(90deg, rgba(2,0,36,0) 0%, #0700b8 52%, #00ff88 100%);\">\n" +
         "            <div class=\"container mx-auto px-6 py-2 flex justify-between items-center\">\n" +
-        "                <a class=\"font-bold text-2xl lg:text-4xl\" href=\"#\">\n" +
-        "                    <img src=\"cdn/Logo_White.png\" width=\"50\">\n" +
+        "                <a class=\"font-bold text-2xl lg:text-4xl\" href=\"/\" style=\"margin-left: -50px\">\n" +
+        "                    <img src=\"cdn/NLogo_White.png\" width=\"305\" class=\"px-0\">\n" +
         "                </a>\n" +
         "                <div class=\"block lg:hidden\">\n" +
         "                    <button class=\"flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-gray-800 hover:border-teal-500 appearance-none focus:outline-none\">\n" +
@@ -25,9 +24,9 @@ Vue.component("navbar", {
         "                </div>\n" +
         "                <div class=\"hidden lg:block\">\n" +
         "                    <ul class=\"inline-flex\">\n" +
-        "                        <li><a class=\"px-4 text-white font-bold\" href=\"/\">Home</a></li>\n" +
-        "                        <li><a class=\"px-4 text-white hover:text-blue-800\" href=\"#\">About</a></li>\n" +
-        "                        <li><a class=\"px-4 text-white hover:text-blue-800\" href=\"#\">Contact</a></li>\n" +
+        "                        <li><a class=\"px-4 text-white hover:text-blue-200\" href=\"/\">Home</a></li>\n" +
+        "                        <li><a class=\"px-4 text-white hover:text-blue-200\" href=\"#\">About</a></li>\n" +
+        "                        <li><a class=\"px-4 text-white hover:text-blue-200\" href=\"/discord\">Discord</a></li>\n" +
         "                    </ul>\n" +
         "                </div>\n" +
         "            </div>\n" +
