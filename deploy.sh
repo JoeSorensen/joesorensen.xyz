27d9bee7243016bad5be20f105305cc8880be1b7
Soren <joseph.md.sorensen@gmail.com>
2019-10-22 19:59:33 -0600

    fixed and cleaned deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,18 @@
 #!/usr/bin/env sh
 
+# abort on errors
 set -e
-<<<<<<< HEAD
-=======
 
-
-# build
->>>>>>> f1a60613be37b2b7006dcf525eefedd460051313
 npm run build
-cd dist
-<<<<<<< HEAD
-git init
-git add -A
-git commit -m 'deploy'
-git push -f git@github.com:JoeSorensen/joesorensen.xyz.git master:gh-pages
-=======
 
+cd dist
+
+git init
 git add -A
 date=$(date)
 git commit -m "deploy on $date"
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push git@github.com:JoeSorensen/joesorensen.xyz.git
+git push -f git@github.com:JoeSorensen/joesorensen.xyz.git master:gh-pages
 
->>>>>>> f1a60613be37b2b7006dcf525eefedd460051313
 cd -
