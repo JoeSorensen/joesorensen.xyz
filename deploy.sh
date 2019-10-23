f1a60613be37b2b7006dcf525eefedd460051313
Soren <joseph.md.sorensen@gmail.com>
2019-10-22 19:46:31 -0600

    deploy on Tue Oct 22 19:46:31 MDT 2019

--- deploy.sh
+++ deploy.sh
@@ -3,23 +3,24 @@
 # abort on errors
 set -e
 
+rm -rf dist/
+
+git clone -b gh-pages https://github.com/JoeSorensen/joesorensen.xyz.git dist
+
 # build
 npm run build
 
 # navigate into the build output directory
 cd dist
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
-git init
 git add -A
-git commit -m 'deploy'
+date=$(date)
+git commit -m "deploy on $date"
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:JoeSorensen/joesorensen.xyz.git master:gh-pages
+git push git@github.com:JoeSorensen/joesorensen.xyz.git
 
 cd -
