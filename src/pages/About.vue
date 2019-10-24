35597d4403a0e582382ba2801ba84f34429898a2
Soren Dangaard <joseph.md.sorensen@gmail.com>
2019-10-24 10:53:32 -0600

    prepare for deploy

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -34,7 +34,6 @@
                     </div>
 
                     <MyWork></MyWork>
-                    <MyWorkArtist></MyWorkArtist>
                 </div>
             </div>
         </section>
