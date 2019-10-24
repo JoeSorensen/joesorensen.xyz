644b0881bcfde2385929798ed88fd2770088e8c6
Soren Dangaard <joseph.md.sorensen@gmail.com>
2019-10-24 08:44:57 -0600

    successfully implemented purgecss (yay!)

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,17 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
-
-    // Specify the paths to all of the template files in your project
-    content: [
-        './src/**/*.vue',
-    ],
-
-    // Include any special characters you're using in this regular expression
-    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+    content: [ './public/**/*.html', './src/**/*.vue' ],
+    defaultExtractor: (content) => {
+        const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '')
+        return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || []
+    },
+    whitelistPatterns: [ /-(leave|enter|appear)(|-(to|from|active))$/, /^(?!cursor-move).+-move$/, /^router-link(|-exact)-active$/ ],
 })
 
 module.exports = {
     plugins: [
         require('tailwindcss'),
-        //purgecss
+        ...process.env.NODE_ENV === 'production'
+            ? [purgecss]
+            : []
     ]
 };
